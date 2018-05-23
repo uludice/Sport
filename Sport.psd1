95ffd4d2a854750e79f70cc964df032db78241c7
uludice <39553716+uludice@users.noreply.github.com>
2018-05-23 13:24:46 +0300

    Add files via upload

---
+++ Sport.psd1
@@ -0,0 +1,21 @@
+@{
+RootModule = 'Sport.psm1'
+ModuleVersion = '1.0'
+GUID = '6a2a8046-b568-4cf7-a877-2dacf3078044'
+Author = 'uludice'
+CompanyName = 'uludice'
+Copyright = '(c) 2018 uludice. All rights reserved.'
+Description = 'TCP port scanner'
+PowerShellVersion = '5.1'
+CmdletsToExport = @()
+FunctionsToExport = 'Sport'
+FileList = 'Sport.psm1'
+AliasesToExport = @()
+    PrivateData = @{
+        PSData = @{
+        LicenseUri = 'http://www.apache.org/licenses/LICENSE-2.0';
+        ProjectUri = 'https://github.com/uludice/Sport'
+        }
+    }
+}
+
---
+++ Sport.psm1
@@ -0,0 +1,74 @@
+﻿function Sport {
+<# 
+.SYNOPSIS
+Scan TCP port on remote host.
+
+.DESCRIPTION
+Sport try to connect to TCP-port on specified host.
+
+.PARAMETER ComputerName
+Host or ip address.
+
+.PARAMETER Port
+Port number.
+
+.PARAMETER Timeout
+Connection timeout in milliseconds.
+
+.OUTPUTS 
+System.Boolean
+
+Function returns $TRUE, if port opened, $FALSE if port closed or $NULL when any exception occurs.
+
+.EXAMPLE
+sport localhost 445
+# Test port 445 on localhost.
+.EXAMPLE
+
+sport -Port 445 -ComputerName localhost -Verbose
+# Same as above, but order of parameters was swapped and vrebose output requested.
+
+.NOTES
+Simplest inline version of our script may be helpful:
+
+function stp($c, $p) { try { $s=[Net.Sockets.TcpClient]::new($c,$p); $s.Connected; $s.close() } catch { $FALSE } }
+#>
+
+[CmdletBinding()]
+
+param (
+    [String]$ComputerName = "localhost", 
+    [Int]$Port = 135,
+    [Int]$Timeout = 100
+)
+
+    if ($PSBoundParameters.ContainsKey("Verbose") -and $PSBoundParameters['Verbose']) {
+        $VerbosePreference = "Continue"
+    }
+
+    $Socket = [Net.Sockets.TcpClient]::new()
+
+    try {
+        Write-Verbose "Test ${ComputerName}:${Port}, timeout ${Timeout}"
+        $WaitHandleResult = $Socket.BeginConnect($ComputerName, $Port, $NULL, $NULL).AsyncWaitHandle.WaitOne($Timeout)
+        if (-not $WaitHandleResult) {
+            Write-Verbose "Timeout occurs"
+        }
+        $Result = $Socket.Connected 
+    } catch { 
+        $Result = $NULL
+        Write-Verbose "Fail"
+        throw $_.exception 
+    } 
+
+    $Socket.close()
+
+    if ($Result) {
+        Write-Verbose "Port opened."
+    } else {
+        Write-Verbose "Port closed."
+    }
+     
+    return $Result
+}
+
